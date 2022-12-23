3f3076aa7cca1690781fb4ca7b262e3dcf6d6d5f
werywjw <werywjw@gmail.com>
2022-12-24 00:31:46 +0100

    updated listnet

--- listnet/trainer.py
+++ listnet/trainer.py
@@ -5,9 +5,28 @@ from sklearn.metrics import average_precision_score
 
 keras = tf.keras
 
-from tf_listnet.models import ListNet
+#from listnet.models import ListNet #???ModuleNotFoundError: No module named 'listnet'
 
 FEATURE_LENGTH = 46
+
+class ListNet(keras.Model):
+    def __init__(self):
+        super(ListNet, self).__init__(self)
+        l2_reg = 0.01
+
+        self.dense1 = keras.layers.Dense(
+            32, activation="relu", kernel_regularizer=keras.regularizers.l2(l2_reg)
+        )
+        self.dense2 = keras.layers.Dense(
+            32, activation="relu", kernel_regularizer=keras.regularizers.l2(l2_reg)
+        )
+        self.dense_output = keras.layers.Dense(1)
+
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        x = self.dense2(x)
+
+        return self.dense_output(x)
 
 
 def _parse_function(example_proto):
